3feaccfa5dc6a3d209cea56343cff0b3473526a6
wingkwong <wingkwong.code@gmail.com>
2019-01-30 22:47:24 +0800

    added height & formatted

--- lib/src/rich_link_preview.dart
+++ lib/src/rich_link_preview.dart
@@ -3,10 +3,12 @@ import './rich_link_preview_view.dart';
 
 class RichLinkPreview extends StatefulWidget {
   RichLinkPreview({
-      this.link
+      this.link,
+      this.height
   });
 
   final String link;
+  final double height;
 
   @override
   RichLinkPreviewView createState() => RichLinkPreviewView();
--- lib/src/rich_link_preview_model.dart
+++ lib/src/rich_link_preview_model.dart
@@ -3,10 +3,12 @@ import 'package:flutter/services.dart';
 import 'package:open_graph_parser/open_graph_parser.dart';
 import './rich_link_preview.dart';
 
-abstract class RichLinkPreviewModel extends State<RichLinkPreview> with SingleTickerProviderStateMixin{
+abstract class RichLinkPreviewModel extends State<RichLinkPreview>
+    with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation<Offset> position;
   String _link;
+  double _height;
   Map _ogData;
 
   getOGData() async {
@@ -16,11 +18,11 @@ abstract class RichLinkPreviewModel extends State<RichLinkPreview> with SingleTi
         print(_ogData);
       });
 
-      controller =
-          AnimationController(vsync: this, duration: Duration(milliseconds: 750));
+      controller = AnimationController(
+          vsync: this, duration: Duration(milliseconds: 750));
       position = Tween<Offset>(begin: Offset(0.0, 4.0), end: Offset.zero)
           .animate(
-          CurvedAnimation(parent: controller, curve: Curves.bounceInOut));
+              CurvedAnimation(parent: controller, curve: Curves.bounceInOut));
 
       controller.forward();
     });
@@ -30,94 +32,77 @@ abstract class RichLinkPreviewModel extends State<RichLinkPreview> with SingleTi
   void initState() {
     super.initState();
     _link = widget.link ?? '';
+    _height = widget.height ?? 100.0;
     getOGData();
   }
 
-  void _onLinkChange(String link) {
-
-  }
+  void _onLinkChange(String link) {}
 
   Widget buildRichLinkPreview(BuildContext context) {
-    if(_ogData == null) {
-      return (
-          null
-      );
+    if (_ogData == null) {
+      return (null);
     } else {
-      return (
-          SlideTransition(
-              position: position,
-              child: Container(
-                  height: 100.0,
-                  decoration: new BoxDecoration (
-                      color: Colors.green,
-                      borderRadius: const BorderRadius.all(const Radius.circular(10.0)),
-                      ),
-                  child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Expanded(
-                            flex: 2,
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: <Widget>[
-                                  Container(
-                                      child: new ClipRRect(
-                                          borderRadius: const BorderRadius.only(
-                                              topLeft: Radius.circular(10.0),
-                                              bottomLeft: Radius.circular(10.0)
-                                          ),
-                                          child: Image.network(
-                                              _ogData['image:secure_url'],
-                                              width: 120.0,
-                                              height: 100.0,
-                                              fit: BoxFit.fill
-                                          )
-                                      )
-                                  )
-                                ],
-                                )
-                        ),
-                        Expanded(
-                            flex: 5,
-                            child: _buildRichLinkPreviewBody(context, _ogData)
-                        ),
-                      ]
-                  )
-              )
-          )
-
-      );
+      return (SlideTransition(
+          position: position,
+          child: Container(
+              height: _height,
+              decoration: new BoxDecoration(
+                color: Colors.green,
+                borderRadius:
+                    const BorderRadius.all(const Radius.circular(10.0)),
+              ),
+              child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 2,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Container(
+                                child: new ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(10.0),
+                                        bottomLeft: Radius.circular(10.0)),
+                                    child: Image.network(
+                                        _ogData['image:secure_url'],
+                                        width: 120.0,
+                                        height: _height,
+                                        fit: BoxFit.fill)))
+                          ],
+                        )),
+                    Expanded(
+                        flex: 5,
+                        child: _buildRichLinkPreviewBody(context, _ogData)),
+                  ]))));
     }
   }
 
   Widget _buildRichLinkPreviewBody(BuildContext context, Map _ogData) {
-    return (
-        Container (
-            height: 100.0,
-            color: Colors.yellow,
-            child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(1.0),
-                    child:  new Text(_ogData['title'])
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(2.0),
-                    child: new Text(_ogData['description'],
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 3,)
-                  ),
-                  Padding(
-                      padding: EdgeInsets.all(2.0),
-                      child: new Text(_ogData['url'],
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 3,)
-                  )
-                ]
-            )
-        )
-    );
+    return (Container(
+        height: _height,
+        color: Colors.yellow,
+        child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Padding(
+                  padding: EdgeInsets.all(1.0),
+                  child: new Text(_ogData['title'])),
+              Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: new Text(
+                    _ogData['description'],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  )),
+              Padding(
+                  padding: EdgeInsets.all(2.0),
+                  child: new Text(
+                    _ogData['url'],
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ))
+            ])));
   }
 }
